564eb141bd6e7062610bd988c08d3e0eacd20e28
nanchacha <csw449@gmail.com>
2024-08-18 00:25:11 +0900

    신용잔고 추가

--- stockInfo.py
+++ stockInfo.py
@@ -3,6 +3,9 @@ from datetime import datetime,timedelta
 import urllib3
 import streamlit as st
 import pandas as pd
+import json
+
+st.markdown("<h1 style='text-align: center; color: red;'>최근 상한가</h1>", unsafe_allow_html=True)
 
 resultList = []
 
@@ -29,7 +32,21 @@ def getStockInfo(baseDate):
         for i in range(length):
             if (float(data['response']['body']['items']['item'][i]['fltRt']) > 29.7):
                 # print(baseDate, data['response']['body']['items']['item'][i]['itmsNm'], data['response']['body']['items']['item'][i]['fltRt'])
-                resultList.append([baseDate,data['response']['body']['items']['item'][i]['itmsNm'], data['response']['body']['items']['item'][i]['fltRt']])   
+                resultList.append([baseDate,data['response']['body']['items']['item'][i]['itmsNm'], data['response']['body']['items']['item'][i]['fltRt']])
+
+
+def getCreditStatus():
+
+    url = 'https://apis.data.go.kr/1160100/service/GetKofiaStatisticsInfoService/getGrantingOfCreditBalanceInfo?serviceKey=bvaKtjYJhZTaGf%2FnaWnOgKRvWFZt90rfTuu1nUWwaVNqxTkJLWSrKT9oIw0AGoD5IiNaBUbdy%2BEgypnlZ%2ByZ6Q%3D%3D&numOfRows=1&pageNo=1&resultType=json'
+    r = requests.get(url, verify=False)
+    if r.status_code == 200:
+        data = r.json()
+        print(data)
+        # crdTrFingWhl 값 추출
+        crdTrFingWhl_value = data['response']['body']['items']['item'][0]['crdTrFingWhl']
+        print("crdTrFingWhl:", crdTrFingWhl_value)
+
+        return str(crdTrFingWhl_value)
 
 now = datetime.now()
 today = now.date()
@@ -43,6 +60,25 @@ for i in range(10, 0, -1):
 frontData = pd.DataFrame(resultList, columns=['날짜', '종목명', '등락률'])
 frontData.set_index('날짜', inplace=True)
 
-st.markdown("<h1 style='text-align: center; color: red;'>최근 상한가</h1>", unsafe_allow_html=True)
-st.table(frontData)
+col1, col2  = st.columns(2)
+
+with col1:
+    # now = datetime.now()
+    # today = now.date()
+    # for i in range(10, 0, -1):
+    #     dTime = str(timeCount(now, i).date()).replace('-','')
+    #     # print(dTime)
+    #     getStockInfo(dTime)
+
+    # frontData = pd.DataFrame(resultList, columns=['날짜', '종목명', '등락률'])
+    # frontData.set_index('날짜', inplace=True)
+
+    st.table(frontData)
+
+with col2:
+    st.write("{}".format(getCreditStatus()))
+
+
+
+# st.table(frontData)
 
